1ac3340d6dfafb19bd78738bb90bc8fd161985bc
Archie Maskill <archie.maskill@gmail.com>
2013-08-13 20:41:30 +0100

    Still converting code from Python.  Still not compiling.

--- src/pacman/AnyFoodSearchProblem.java
+++ src/pacman/AnyFoodSearchProblem.java
@@ -1,18 +1,15 @@
 package pacman;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import util.Position;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 
 /**
- *
- * @author archie
- */
-public class AnyFoodSearchProblem {
-class AnyFoodSearchProblem(PositionSearchProblem):
-    """
-      A search problem for finding a path to any food.
+ * A search problem for finding a path to any food.
 
       This search problem is just like the PositionSearchProblem, but
       has a different goal test, which you need to fill in below.  The
@@ -23,27 +20,31 @@ class AnyFoodSearchProblem(PositionSearchProblem):
 
       You can use this search problem to help you fill in
       the findPathToClosestDot method.
-    """
-
-    def __init__(self, gameState):
-        "Stores information from the gameState.  You don't need to change this."
-        # Store the food for later reference
-        self.food = gameState.getFood()
-
-        # Store info for the PositionSearchProblem (no need to change this)
-        self.walls = gameState.getWalls()
-        self.startState = gameState.getPacmanPosition()
-        self.costFn = lambda x: 1
-        self._visited, self._visitedlist, self._expanded = {}, [], 0
-
-    def isGoalState(self, state):
-        """
-        The state is Pacman's position. Fill this in with a goal test
-        that will complete the problem definition.
-        """
-        x,y = state
-
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+ * @author archie
+ */
+public class AnyFoodSearchProblem extends PositionSearchProblem {
     
+    private final Grid food;
+
+    /** Stores information from the gameState.  You don't need to change this. */
+    public AnyFoodSearchProblem(final GameState1 gameState) {
+        super(gameState, new CostFunctionAlwaysOne(), null, gameState.getPacmanPosition(), null, null);
+        // Store the food for later reference
+        this.food = gameState.getFood();
+    }
+
+    /**
+     * The state is Pacman's position. Fill this in with a goal test
+       that will complete the problem definition.
+     */ 
+    public boolean isGoalState(final GameState1 state) {
+        final Position pos = state.getPacmanPosition();
+        final int x = pos.getX();
+        final int y = pos.getY();
+
+        /**
+         * YOUR CODE HERE
+         */
+        throw new NotImplementedException();
+    }
 }
