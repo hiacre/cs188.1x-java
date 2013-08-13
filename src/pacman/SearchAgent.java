97a782d46e208a1cbce7f0cb4df1c43029ed48ee
Archie Maskill <archie.maskill@gmail.com>
2013-08-13 20:28:28 +0100

    Still converting code from Python.  Still not compiling.

--- src/pacman/SearchAgent.java
+++ src/pacman/SearchAgent.java
@@ -4,6 +4,8 @@
  */
 package pacman;
 
+import java.util.List;
+
 /**
  * This very general search agent finds a path using a supplied search algorithm for a
     supplied search problem, then returns actions to follow that path.
@@ -18,74 +20,97 @@ package pacman;
     Note: You should NOT change any code in SearchAgent
  * @author archie
  */
-class SearchAgent {
+public class SearchAgent {
+    
+    private final Object searchFunction;
+    private int actionIndex;
+    private final List<Direction> actions;
+    private final Object searchType;
+    private final Object heur;
 
-    public SearchAgent(final Object fn='depthFirstSearch', prob='PositionSearchProblem', heuristic='nullHeuristic') {
+    public SearchAgent(final Object fn, final Object prob, final Object heuristic) {
         
         if(fn == null) {
-            fn = DepthFirstSearch;
+            fn = new DepthFirstSearch();
         }
         if(prob == null) {
-            prob = PositionSearchProblem;
+            prob = new PositionSearchProblem();
         }
         if(heuristic == null) {
             heuristic = nullHeuristic;
         }
         
+        this.searchFunction = fn;
+        this.searchType = prob;
+        this.heur = heuristic;
+        
+        this.actionIndex = 0;
+        
+        //final Object func = getattr(search, fn)
+//        if 'heuristic' not in func.func_code.co_varnames:
+//            print('[SearchAgent] using function ' + fn)
+//            self.searchFunction = func
+//        else:
+//            if heuristic in globals().keys():
+//                heur = globals()[heuristic]
+//            elif heuristic in dir(search):
+//                heur = getattr(search, heuristic)
+//            else:
+//                raise AttributeError, heuristic + ' is not a function in searchAgents.py or search.py.'
+//            print('[SearchAgent] using function %s and heuristic %s' % (fn, heuristic))
+//            # Note: this bit of Python trickery combines the search algorithm and the heuristic
+//            self.searchFunction = lambda x: func(x, heuristic=heur)
+        
 
-        // Get the search function from the name and heuristic
-        if fn not in dir(search):
-            raise AttributeError, fn + ' is not a search function in search.py.'
-        func = getattr(search, fn)
-        if 'heuristic' not in func.func_code.co_varnames:
-            print('[SearchAgent] using function ' + fn)
-            self.searchFunction = func
-        else:
-            if heuristic in globals().keys():
-                heur = globals()[heuristic]
-            elif heuristic in dir(search):
-                heur = getattr(search, heuristic)
-            else:
-                raise AttributeError, heuristic + ' is not a function in searchAgents.py or search.py.'
-            print('[SearchAgent] using function %s and heuristic %s' % (fn, heuristic))
-            # Note: this bit of Python trickery combines the search algorithm and the heuristic
-            self.searchFunction = lambda x: func(x, heuristic=heur)
+//        # Get the search problem type from the name
+//        if prob not in globals().keys() or not prob.endswith('Problem'):
+//            raise AttributeError, prob + ' is not a search problem type in SearchAgents.py.'
+//        self.searchType = globals()[prob]
+//        print('[SearchAgent] using problem type ' + prob)
+    }
 
-        # Get the search problem type from the name
-        if prob not in globals().keys() or not prob.endswith('Problem'):
-            raise AttributeError, prob + ' is not a search problem type in SearchAgents.py.'
-        self.searchType = globals()[prob]
-        print('[SearchAgent] using problem type ' + prob)
-
-    def registerInitialState(self, state):
-        """
-        This is the first time that the agent sees the layout of the game board. Here, we
+    
+    /** 
+     * This is the first time that the agent sees the layout of the game board. Here, we
         choose a path to the goal.  In this phase, the agent should compute the path to the
         goal and store it in a local variable.  All of the work is done in this method!
 
         state: a GameState object (pacman.py)
-        """
-        if self.searchFunction == None: raise Exception, "No search function provided for SearchAgent"
-        starttime = time.time()
-        problem = self.searchType(state) # Makes a new search problem
-        self.actions  = self.searchFunction(problem) # Find a path
-        totalCost = problem.getCostOfActions(self.actions)
-        print('Path found with total cost of %d in %.1f seconds' % (totalCost, time.time() - starttime))
-        if '_expanded' in dir(problem): print('Search nodes expanded: %d' % problem._expanded)
+     * @param state 
+     */
+    public void registerInitialState(final Object state) {
 
-    def getAction(self, state):
-        """
-        Returns the next action in the path chosen earlier (in registerInitialState).  Return
+        if(this.searchFunction == null) {
+            throw new RuntimeException("No search function provided for SearchAgent");
+        }
+        final long starttime = System.currentTimeMillis();
+        final Object problem = this.searchType(state); // Makes a new search problem
+        this.actions = this.searchFunction(problem);  // Find a path
+        final int totalCost = problem.getCostOfActions(this.actions);
+        final double duration = (starttime - System.currentTimeMillis())/1000;
+        System.out.println("Path found with total cost of " + totalCost + " in " + duration + " seconds");
+        System.out.println("Search nodes expanded: " + problem.getExpanded());
+    }
+
+    
+    /**
+     * Returns the next action in the path chosen earlier (in registerInitialState).  Return
         Directions.STOP if there is no further action to take.
 
         state: a GameState object (pacman.py)
-        """
-        if 'actionIndex' not in dir(self): self.actionIndex = 0
-        i = self.actionIndex
-        self.actionIndex += 1
-        if i < len(self.actions):
-            return self.actions[i]
-        else:
-            return Directions.STOP
+     * @param state
+     * @return 
+     */
+    public Direction getAction(final GameState1 state) {
+        
+        final int i = actionIndex;
+        this.actionIndex += 1;
+        if(i < this.actions.size()) {
+            return this.actions.get(i);
+        }
+        else {
+            return Direction.Stop;
+        }
+    }
 
 }
