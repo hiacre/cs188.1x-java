dafdda2163596133a979140f409d4948a01c7aa3
Archie Maskill <archie.maskill@gmail.com>
2013-08-31 23:01:54 +0100

    Conversion of code used for graphical display.  Created a new project containing just the Position class, because two projects need it, and need to avoid circular dependency.

---
+++ common/src/common/Position.java
@@ -0,0 +1,56 @@
+package common;
+
+
+
+public class Position {
+
+    private final double x;
+    private final double y;
+    
+    private Position(final double x, final double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public static Position newInstance(final double x, final double y) {
+        return new Position(x, y);
+    }
+    
+    /**
+     * Finds the nearest grid point to a position (discretizes)
+     */
+    public static Position nearestPoint(final double x, final double y) {
+        final int gridRow = (int)(x+0.5);
+        final int gridCol = (int)(y+0.5);
+        return new Position(gridRow, gridCol);
+    }
+    
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double manhattanDistance(final Position point) {
+        return Math.abs(getX()-point.getX()) + Math.abs(getY()-point.getY());
+    }
+
+    public int getRoundedX() {
+        return (int)(x+0.5);
+    }
+
+    public int getRoundedY() {
+        return (int)(y+0.5);
+    }
+
+    public int getFloorX() {
+        return (int)x;
+    }
+
+    public int getFloorY() {
+        return (int)y;
+    }
+    
+}
