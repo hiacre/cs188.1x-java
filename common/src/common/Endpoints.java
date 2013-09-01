2794295a22a0346f76796136032bb7cce43df173
Archie Maskill <archie.maskill@gmail.com>
2013-09-01 11:57:04 +0100

    Conversion of code used for graphical display.

---
+++ common/src/common/Endpoints.java
@@ -0,0 +1,24 @@
+package common;
+
+/**
+ *
+ * @author archie
+ */
+public class Endpoints {
+
+    final double first;
+    final double second;
+    
+    public Endpoints(double first, double second) {
+        this.first = first;
+        this.second = second;
+    }
+    
+    public double getFirst() {
+        return first;
+    }
+    
+    public double getSecond() {
+        return second;
+    }
+}
